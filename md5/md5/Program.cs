41ce353f6c624e5c3ab6e9d13e08286a61987a5b
Shaun Xu <jfarrio@gmail.com>
2013-09-06 13:30:55 +0800

    init

---
+++ md5/md5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace md5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine("Usage: md5 [file name]");
+                return;
+            }
+
+            var md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            using (var fs = File.OpenRead(args[0]))
+            {
+                var hash = md5.ComputeHash(fs);
+                var value = Convert.ToBase64String(hash);
+                Console.WriteLine(string.Format("{0} > {1}", args[0], value));
+            }
+        }
+    }
+}
---
+++ rngf/rngf/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rngf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length < 2)
+            {
+                Console.WriteLine("Usage: rngf [file size in mb] [file name]");
+                return;
+            }
+
+            var sizeInMB = int.Parse(args[0]);
+            var filename = args[1];
+
+            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            using (var fs = File.OpenWrite(filename))
+            {
+                for (var i = 0; i < sizeInMB; i++)
+                {
+                    var buffer = new byte[1024 * 1024];
+                    rng.GetBytes(buffer);
+                    fs.Write(buffer, 0, buffer.Length);
+                }
+            }
+
+            Console.WriteLine(string.Format("Output '{0}' ({1}MB).", filename, sizeInMB));
+        }
+    }
+}
