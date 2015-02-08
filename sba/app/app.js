28ab26ca3478943d0ed7df42a7c5f72f3270a2ab
Shaun Xu <jfarrio@gmail.com>
2015-02-08 22:19:11 +0800

    2

--- sba/app/app.js
+++ sba/app/app.js
@@ -1,76 +1,33 @@
-var app = angular.module('app', [ 'kendo.directives' ]);
-
-app.controller('MainCtrl', function ($scope, $http) {
-    $scope.validatorOptions = {
-        rules: {
-            'greater-date': function (input) {
-                if (input.is('[id=end]')) {
-                    return !$scope.end || !$scope.start || ($scope.end > $scope.start);
-                }
-                return true;
-            },
-            'job-name-duplicated': function (input) {
-                if (input.is('[id=jobname')) {
-                    var thisName = $scope.jobname;
-                    if (thisName) {
-                        $http.get('api/jobs.json')
-                            .success(function (jobs) {
-                                var duplicated = false;
-                                jobs.forEach(function (job) {
-                                    if (thisName === job) {
-                                        duplicated = true;
-                                    }
-                                });
-                                var result = !duplicated;
-                                return result;
-                            })
-                            .error(function () {
-                                return false;
-                            });
+(function(angular) {
+    'use strict';
+    angular.module('customTriggerExample', ['ngMessages', 'ui.bootstrap'])
+        .controller('ExampleController', ['$scope', function($scope) {
+            $scope.user = {};
+            $scope.getErrorMessage = function (name) {
+                var control = $scope.form[name];
+                if (control) {
+                    if (control.$dirty && control.$invalid) {
+                        var message = '';
+                        Object.getOwnPropertyNames(control.$error).forEach(function (key) {
+                            if ($scope.errorMessages[name][key]) {
+                                message = $scope.errorMessages[name][key];
+                            }
+                        });
+                        return message;
                     }
                     else {
-                        return true;
+                        return undefined;
                     }
                 }
                 else {
-                    return true;
+                    return undefined;
                 }
-            }
-        },
-        messages: {
-            'job-name-duplicated': function (input) {
-                return 'Duplicated job name "' + input.val() + '" for {0}';
-            }
-        }
-    };
-
-    $scope.waveCountOptions = {
-        format: '#',
-        min: 1,
-        max: 100,
-        step: 1
-    };
-
-    $scope.validate = function(event) {
-        $scope.data = [
-            "12 Angry Men",
-            "Il buono, il brutto, il cattivo.",
-            "Inception",
-            "One Flew Over the Cuckoo's Nest",
-            "Pulp Fiction",
-            "Schindler's List",
-            "The Dark Knight",
-            "The Godfather",
-            "The Godfather: Part II",
-            "The Shawshank Redemption"
-        ];
-
-        event.preventDefault();
-
-        if ($scope.jobValidator.validate()) {
-            $scope.validationMessage = "Awesome! Your job information is valid.";
-        } else {
-            $scope.validationMessage = "Oops! Something wrong here.";
-        }
-    }
-});
+            };
+            $scope.errorMessages = {
+                'uName': {
+                    'required': 'Name is missing.',
+                    'minlength': 'Name must be more than 2 chars.'
+                }
+            };
+        }]);
+})(window.angular);
